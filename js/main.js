084086015a7cf78715cc589bced151de18be6b22
adevlinb <devlinbgenassembly@gmail.com>
2022-06-27 13:09:58 -0700

    initializes state

--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
 /*----- constants -----*/
-
+const WORDS = [
+    'black hole',
+    'planets',
+    'space force',
+    'light speed',
+    'interstellar',
+    'exploration',
+    'cosmic',
+    'sputnik',
+    'gravity',
+    'asteroid',
+    'galaxy',
+    'star'
+];
 
 
 
 
 
 /*----- app's state (variables) -----*/
-
-
-
-
+let wrongGuesses; //array to hold incorrect letters
+let winner; // null -> game in progress; "W" -> win, "L" -> lost
+let secret; // array of the chars for the randomly selected word
+let guess; // array of current guessed letters
 
 /*----- cached element references -----*/
 
@@ -23,3 +36,17 @@
 
 
 /*----- functions -----*/
+
+function init() {
+    wrongGuesses = [];
+    winner = null;
+    const randomIdx = Math.floor(Math.random() * WORDS.length);
+    secret = WORDS[randomIdx].split("")
+    guess = secret.map((letter) => letter === " " ? " " : "_");
+    winner = null;
+    render();
+}
+
+function render() {
+    
+}
