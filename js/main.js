b85b9536396c2994fc295ff9daf38e19a4847fda
adevlinb <devlinbgenassembly@gmail.com>
2022-06-27 14:49:14 -0700

    update

--- js/main.js
+++ js/main.js
@@ -15,61 +15,79 @@ const WORDS = [
     'star'
 ];
 
-
-
-
 /*----- app's state (variables) -----*/
-let wrongGuesses; //array to hold incorrect letters
-let secret; // array of the chars for the randomly selected word
-let guess; // array of current guessed letters
-let gameStatus; // null -> game in progress; "W" -> win, "L" -> lost
+let wrongGuesses;  // Array to hold incorrect letters
+let secret;  // Array of the chars for the randomly selected word
+let guess;  // Array of current guessed letters
+let gameStatus;  // null -> game in progress; 'W' -> won; 'L' -> lost
 
 /*----- cached element references -----*/
-const replayBtn = document.getElementById("play-again-btn");
-const guessEl = document.getElementById("guess")
-const spacemanImg = document.querySelector('img')
-
-
+const replayBtn = document.getElementById('play-again-btn');
+const guessEl = document.getElementById('guess');
+const spacemanImg = document.querySelector('img');
+const letterBtns = [...document.querySelectorAll("article > button")]
 
 /*----- event listeners -----*/
-document.querySelector("article")
-    .addEventListener("click", handleLetterClick);
-
-
+document.querySelector('article')
+    .addEventListener('click', handleLetterClick);
 
 /*----- functions -----*/
+init();
 
 function init() {
     wrongGuesses = [];
-    const randomIdx = Math.floor(Math.random() * WORDS.length);
-    secret = WORDS[randomIdx].split("")
-    guess = secret.map((letter) => letter === " " ? " " : "_");
+    const rndIdx = Math.floor(Math.random() * WORDS.length);
+    secret = WORDS[rndIdx].toUpperCase().split('');
+    // map always returns a NEW array of the same # of elements
+    guess = secret.map(ltr => ltr === ' ' ? ' ' : '_');
     gameStatus = null;
+    console.log("hello", secret)
     render();
 }
 
 function render() {
     // render the message
-    spacemanImg.src = `imgs/spaceman-${0}.jpg`
-    guessEl.textContent = guess.join("")
+    // render the spaceman
+    spacemanImg.src = `images/spaceman-${wrongGuesses.length}.jpeg`;
+    // render the guess
+    guessEl.textContent = guess.join('');
     // render the buttons
     renderButtons();
-
-
 }
 
 function renderButtons() {
-    replayBtn.style.visibility = winner ? 'visible' : 'hidden';
+    letterBtns.forEach(function(btn) {
+        ltr = btn.textContent
+        if (wrongGuesses.includes(ltr)) {
+            btn.className = "wrong";
+        } else if (guess.includes(ltr)) {
+            btn.className = "correct";
+        } else {
+            btn.className = "";
+        }
+    })
+    replayBtn.style.visibility = gameStatus ? 'visible' : 'hidden';
 }
 
-function handleLetterClick(e) {
-    const letter = e.target.textContent;
-    //guards if game is over
-    if(
-        gameStatus || 
-        e.target.tabName !== "BUTTON" ||
-        wrongGuesses.includes(letter) ||
-        guessArray.includes(letter) ||
-    ) return
+function handleLetterClick(evt) {
+    const ltr = evt.target.textContent;
+    // guards
+    if (
+        gameStatus ||
+        evt.target.tagName !== 'BUTTON' ||
+        wrongGuesses.includes(ltr) ||
+        guess.includes(ltr)
+    ) return;
+
+    if (secret.includes(ltr)) {
+        //correct guess
+        secret.forEach(function(char, idx) {
+            if (char === ltr) guess[idx] = ltr
+        });
+    } else {
+        //wrong guess
+        wrongGuesses.push(ltr);
+    }
+    render();
 
 }
